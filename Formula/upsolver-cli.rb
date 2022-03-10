9c30be579cfc5d4c0b25aefd9d4bd63ae1d3dbab
alexyavo <alex@upsolver.com>
2022-03-10 10:27:00 +0200

    brew formula update & gen script,

--- Formula/upsolver-cli.rb
+++ Formula/upsolver-cli.rb
@@ -1,29 +1,87 @@
-class Upsolver < Formula
+class UpsolverCli < Formula
   include Language::Python::Virtualenv
 
   desc "CLI for Upsolver with auto-completion and syntax highlighting"
   homepage "https://upsolver.com/"
-  url "https://github.com/Upsolver/cli/releases/download/beta/cli-0.1.0.tar.gz"
-  sha256 "96c440b89c364b661f66ef96d09858d5d88d9eb89b35af55eae712e4df3180b5"
+  url "https://github.com/Upsolver/cli/releases/download/stable/cli-0.1.0.tar.gz"
+  sha256 "9f4bed13bb84187dd86ec29fa8a42afa89cffb094517e3a01664d0bde59a01eb"
   license "BSD-3-Clause"
 
   depends_on "poetry" => :build
   depends_on "python@3.10"
 
-  resource "upsolver-cli" do
-    url "https://github.com/Upsolver/cli/releases/download/beta/cli-0.1.0.tar.gz"
+  resource "certifi" do
+    url "https://files.pythonhosted.org/packages/6c/ae/d26450834f0acc9e3d1f74508da6df1551ceab6c2ce0766a593362d6d57f/certifi-2021.10.8.tar.gz"
+    sha256 "78884e7c1d4b00ce3cea67b44566851c4343c120abd683433ce934a68ea58872"
+  end
+
+  resource "charset-normalizer" do
+    url "https://files.pythonhosted.org/packages/56/31/7bcaf657fafb3c6db8c787a865434290b726653c912085fbd371e9b92e1c/charset-normalizer-2.0.12.tar.gz"
+    sha256 "2857e29ff0d34db842cd7ca3230549d1a697f96ee6d3fb071cfa6c7393832597"
+  end
+
+  resource "click" do
+    url "https://files.pythonhosted.org/packages/dd/cf/706c1ad49ab26abed0b77a2f867984c1341ed7387b8030a6aa914e2942a0/click-8.0.4.tar.gz"
+    sha256 "8458d7b1287c5fb128c90e23381cf99dcde74beaf6c7ff6384ce84d6fe090adb"
+  end
+
+  resource "idna" do
+    url "https://files.pythonhosted.org/packages/62/08/e3fc7c8161090f742f504f40b1bccbfc544d4a4e09eb774bf40aafce5436/idna-3.3.tar.gz"
+    sha256 "9d643ff0a55b762d5cdb124b8eaa99c66322e2157b69160bc32796e824360e6d"
+  end
+
+  resource "multidict" do
+    url "https://files.pythonhosted.org/packages/fa/a7/71c253cdb8a1528802bac7503bf82fe674367e4055b09c28846fdfa4ab90/multidict-6.0.2.tar.gz"
+    sha256 "5ff3bd75f38e4c43f1f470f2df7a4d430b821c4ce22be384e1459cb57d6bb013"
+  end
+
+  resource "prompt-toolkit" do
+    url "https://files.pythonhosted.org/packages/37/34/c34c376882305c5051ed7f086daf07e68563d284015839bfb74d6e61d402/prompt_toolkit-3.0.28.tar.gz"
+    sha256 "9f1cd16b1e86c2968f2519d7fb31dd9d669916f515612c269d14e9ed52b51650"
+  end
+
+  resource "Pygments" do
+    url "https://files.pythonhosted.org/packages/94/9c/cb656d06950268155f46d4f6ce25d7ffc51a0da47eadf1b164bbf23b718b/Pygments-2.11.2.tar.gz"
+    sha256 "4e426f72023d88d03b2fa258de560726ce890ff3b630f88c21cbb8b2503b8c6a"
+  end
+
+  resource "requests" do
+    url "https://files.pythonhosted.org/packages/60/f3/26ff3767f099b73e0efa138a9998da67890793bfa475d8278f84a30fec77/requests-2.27.1.tar.gz"
+    sha256 "68d7c56fd5a8999887728ef304a6d12edc7be74f1cfa47714fc8b414525c9a61"
+  end
+
+  resource "simplejson" do
+    url "https://files.pythonhosted.org/packages/7a/47/c7cc3d4ed15f09917838a2fb4e1759eafb6d2f37ebf7043af984d8b36cf7/simplejson-3.17.6.tar.gz"
+    sha256 "cf98038d2abf63a1ada5730e91e84c642ba6c225b0198c3684151b1f80c5f8a6"
+  end
+
+  resource "tabulate" do
+    url "https://files.pythonhosted.org/packages/ae/3d/9d7576d94007eaf3bb685acbaaec66ff4cdeb0b18f1bf1f17edbeebffb0a/tabulate-0.8.9.tar.gz"
+    sha256 "eb1d13f25760052e8931f2ef80aaf6045a6cceb47514db8beab24cded16f13a7"
+  end
+
+  resource "typing-extensions" do
+    url "https://files.pythonhosted.org/packages/b1/5a/8b5fbb891ef3f81fc923bf3cb4a578c0abf9471eb50ce0f51c74212182ab/typing_extensions-4.1.1.tar.gz"
+    sha256 "1a9462dcc3347a79b1f1c0271fbe79e844580bb598bafa1ed208b94da3cdcd42"
+  end
+
+  resource "urllib3" do
+    url "https://files.pythonhosted.org/packages/b0/b1/7bbf5181f8e3258efae31702f5eab87d8a74a72a0aa78bc8c08c1466e243/urllib3-1.26.8.tar.gz"
+    sha256 "0e7c33d9a63e7ddfcb86780aac87befc2fbddf46c58dbb487e0855f7ceec283c"
+  end
+
+  resource "wcwidth" do
+    url "https://files.pythonhosted.org/packages/89/38/459b727c381504f361832b9e5ace19966de1a235d73cdbdea91c771a1155/wcwidth-0.2.5.tar.gz"
+    sha256 "c4d647b99872929fdb7bdcaa4fbe7f01413ed3d98077df798530e5b04f116c83"
+  end
+
+  resource "yarl" do
+    url "https://files.pythonhosted.org/packages/f6/da/46d1b3d69a9a0835dabf9d59c7eb0f1600599edd421a4c5a15ab09f527e0/yarl-1.7.2.tar.gz"
+    sha256 "45399b46d60c253327a460e99856752009fcee5f5d3c80b2f7c0cae1c38d56dd"
   end
 
   def install
-    venv = virtualenv_create(libexec, "python3")
-
-    resource("upsolver-cli").stage do
-      system Formula["poetry"].opt_bin/"poetry", "build", "--format", "wheel", "--verbose", "--no-interaction"
-      venv.pip_install Dir["dist/cli-*.whl"].first
-    end
-
-    venv.pip_install resources
-    venv.pip_install_and_link buildpath
+    virtualenv_install_with_resources
   end
 
   test do
---
+++ genformula.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+import hashlib
+import subprocess
+import urllib.request
+from pathlib import Path
+from string import Template
+
+import click
+import poet
+import toml
+
+import cli
+
+DEFAULT_ARCHIVE_URL = f'https://github.com/Upsolver/cli/releases/download/stable/cli-{cli.__version__}.tar.gz'
+
+
+@click.command(help='Generates brew Formula based on poetry.lock file')
+@click.option('--cli-archive-url', default=DEFAULT_ARCHIVE_URL)
+@click.option('--compute-hash-from-build', default=False, is_flag=True)
+@click.option('--poetry-lock-path', default=str(Path.cwd() / 'poetry.lock'))
+@click.option('--formula-template-path', default=str(Path.cwd() / 'brew-formula-template'))
+@click.option('--formula-out-path', default=str(Path.cwd() / 'Formula/upsolver-cli.rb'))
+def generate_brew_formula(cli_archive_url: str,
+                          compute_hash_from_build: bool,
+                          poetry_lock_path: str,
+                          formula_template_path: str,
+                          formula_out_path: str) -> None:
+    lock_content = toml.load(poetry_lock_path)
+
+    # poet.py:141: PackageNotInstalledWarning: colorama is not installed so we cannot compute
+    # resources for its dependencies.
+    #   warnings.warn("{} is not installed so we cannot compute "
+    blacklist = ['colorama']
+
+    resources_txt = poet.resources_for([
+        p['name']
+        for p in lock_content['package']
+        if p['category'] == 'main' and p['name'] not in blacklist
+    ])
+
+    cli_archive_url_for_hash = cli_archive_url
+    if compute_hash_from_build:
+        build_res = subprocess.run(["poetry", "build"])
+        if build_res.returncode != 0:
+            print(f'Failed to build project, "poetry build" exited with code {build_res.returncode}')
+            exit(-1)
+
+        cli_archive_url_for_hash = 'file://' + str(Path.cwd() / f'dist/cli-{cli.__version__}.tar.gz')
+
+    with urllib.request.urlopen(cli_archive_url_for_hash) as cli_archive_f:
+        archive_hash = hashlib.sha256(cli_archive_f.read()).hexdigest()
+
+    with open(formula_template_path, 'r') as formula_template_f, \
+         open(formula_out_path, 'w') as formula_out_f:
+        formula_content = Template(formula_template_f.read()).substitute({
+            'url': cli_archive_url,
+            'url_sha256': archive_hash,
+            'resources': resources_txt
+        })
+
+        print(f'Writing following content to {formula_out_path}:\n\n{formula_content}')
+        formula_out_f.write(formula_content)
+
+
+if __name__ == '__main__':
+    generate_brew_formula()
