6601303846404e261974966eea9eec4127d7be88
alexyavo <alex@upsolver.com>
2022-04-04 10:41:52 +0300

    fixed test_get_base_url_no_dns_info

--- tests/test_api_utils.py
+++ tests/test_api_utils.py
@@ -1,8 +1,6 @@
-import pytest
 from requests_mock import Mocker as RequestsMocker
 from yarl import URL
 
-from cli.errors import PrivateApiUnavailable
 from cli.upsolver.api_utils import get_base_url
 from tests.mock_upsolver_rest_api import MockUpsolverRestApi
 
@@ -11,7 +9,5 @@ def test_get_base_url_no_dns_info(requests_mock: RequestsMocker) -> None:
     rest_api_mock = MockUpsolverRestApi(requests_mock)
     rest_api_mock.set_base_url(None)
 
-    with pytest.raises(PrivateApiUnavailable) as ex:
-        get_base_url(URL('http://localhost'), 'token123')
-
-    assert ex.value.base_url == URL('http://localhost')
+    url = URL('http://localhost')
+    assert get_base_url(URL('http://localhost'), 'token123') == url
