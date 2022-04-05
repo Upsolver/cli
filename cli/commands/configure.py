744d1ad921ea8a4dff38b9974d0b92a2fe6d53ae
alexyavo <alex@upsolver.com>
2022-04-05 20:57:35 +0300

    configure sub-command now offers default base_url even if one is not set in config

--- cli/commands/configure.py
+++ cli/commands/configure.py
@@ -2,7 +2,7 @@ import click
 from yarl import URL
 
 from cli.commands.context import CliContext
-from cli.config import Profile
+from cli.config import ConfigurationManager, Profile
 from cli.formatters import OutputFmt
 
 
@@ -13,6 +13,8 @@ def configure(ctx: CliContext) -> None:
     ctx.confman.update_profile(Profile(
         name=profile.name,
         token=click.prompt('API Access Token', profile.token),
-        base_url=URL(click.prompt('API Endpoint', profile.base_url)),
+        base_url=URL(click.prompt('API Endpoint',
+                                  profile.base_url if profile.base_url is not None
+                                  else ConfigurationManager.CLI_DEFAULT_BASE_URL)),
         output=OutputFmt(click.prompt('Default Output Format', profile.output.value))
     ))
