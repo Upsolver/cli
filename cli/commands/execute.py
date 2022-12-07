0435914f5e9caba138cf0d4c4992ac9fc24c95b8
Noga Tzur <noga.tzur@gmail.com>
2022-12-08 00:23:53 +0200

    CLI/Execute: Add file_path & command & token & api-url flags

--- cli/commands/execute.py
+++ cli/commands/execute.py
@@ -5,16 +5,25 @@ import click
 from click import echo
 
 from cli.commands.context import CliContext
+from cli.config import ProfileAuthSettings
+from cli.errors import ConfigErr
 from cli.formatters import Formatter, OutputFmt
-from cli.utils import convert_time_str
+from cli.utils import convert_time_str, parse_url
 
 
 @click.command()
 @click.pass_obj
-@click.argument('expression')
+@click.option('-f', '--file_path', default=None,
+              help='Execute a file.')
+@click.option('-c', '--command', default=None,
+              help='Execute a string sql statement.')
+@click.option('-t', '--token', default=None,
+              help='Token to use. Requires to provide api-url as well.')
+@click.option('-u', '--api-url', default=None,
+              help='URL of Upsolver\'s API. Requires to provide token as well.')
 @click.option('-o', '--output-format', default=None,
               help='The format that the results will be returned in.')
-@click.option('-t', '--timeout', 'timeout_sec', default='10s', callback=convert_time_str,
+@click.option('--timeout', 'timeout_sec', default='10s', callback=convert_time_str,
               help='Timeout setting for pending responses.')
 @click.option('-d', '--dry-run', is_flag=True, default=False,
               help='Validate expression is syntactically valid but don\'t run the command.')
@@ -22,29 +31,19 @@ from cli.utils import convert_time_str
               help='Ignore errors in query responses. Default behavior is to stop on error.')
 def execute(
         ctx: CliContext,
-        expression: str,
+        file_path: Optional[str],
+        command: Optional[str],
+        token: Optional[str],
+        api_url: Optional[str],
         output_format: Optional[str],
         timeout_sec: float,
         dry_run: bool,
         ignore_errors: bool
 ) -> None:
     """
-    Execute a single SQL query
+    Execute a single SQL query, using -f <file_path> or -c <sql_command>
     """
-    expression = expression.strip()
-
-    if expression == '-':
-        expression = click.get_text_stream('stdin').read().strip()
-    else:
-        p = Path(expression)
-        try:  # In case the path is not valid we don't want the command execution to fail so this try is added
-            if p.exists():
-                expression = p.read_text()
-        except OSError:
-            pass
-
-    if len(expression) == 0:
-        return
+    expression = __get_expression(file_path, command)
 
     fmt: Optional[Formatter] = None
     if output_format is not None:
@@ -53,7 +52,14 @@ def execute(
         except ValueError:
             pass
 
-    api = ctx.upsolver_api()
+    if token and api_url:
+        api_url = parse_url(api_url)
+        api = ctx.upsolver_api(ProfileAuthSettings(token, api_url))
+    elif token or api_url:
+        raise ConfigErr("Please provide a token and an api-url.")
+    else:
+        api = ctx.upsolver_api()
+
     if dry_run:
         check_result = api.check_syntax(expression)
         if len(check_result) > 0:
@@ -79,3 +85,17 @@ def execute(
                         ctx.write({'query': q, 'error': str(ex)})
                     else:
                         click.echo(str(ex))
+
+
+def __get_expression(file_path: Optional[str], command: Optional[str]) -> str:
+    if (file_path and command) or (not file_path and not command):
+        raise ConfigErr("Please provide either a file path (using -f flag) "
+                        "or a sql statement (using -c flag).")
+    if file_path:
+        p = Path(file_path)
+        if p.exists():
+            return p.read_text()
+        else:
+            raise ConfigErr("File not found in location: {}".format(file_path))
+    if command:
+        return command.strip()
