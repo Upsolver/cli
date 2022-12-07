095c74a1f38885f0788890abcb1f54a9ba4dc621
Noga Tzur <noga.tzur@gmail.com>
2022-12-08 00:23:53 +0200

    CLI/Execute: Add token & api-url flags

--- cli/commands/execute.py
+++ cli/commands/execute.py
@@ -5,18 +5,24 @@ import click
 from click import echo
 
 from cli.commands.context import CliContext
+from cli.config import ProfileAuthSettings
+from cli.errors import ConfigErr
 from cli.formatters import Formatter, OutputFmt
-from cli.utils import convert_time_str
+from cli.utils import convert_time_str, parse_url
 
 
 @click.command()
 @click.pass_obj
 @click.argument('expression')
+@click.option('-t', '--token', default=None,
+              help='Token to use. Requires to provide api-url as well.')
+@click.option('-u', '--api-url', default=None,
+              help='URL of Upsolver\'s API. Requires to provide token as well.')
 @click.option('-c', '--command', is_flag=True, default=False,
               help='Execute a string sql statement.')
 @click.option('-o', '--output-format', default=None,
               help='The format that the results will be returned in.')
-@click.option('-t', '--timeout', 'timeout_sec', default='10s', callback=convert_time_str,
+@click.option('--timeout', 'timeout_sec', default='10s', callback=convert_time_str,
               help='Timeout setting for pending responses.')
 @click.option('-d', '--dry-run', is_flag=True, default=False,
               help='Validate expression is syntactically valid but don\'t run the command.')
@@ -25,6 +31,8 @@ from cli.utils import convert_time_str
 def execute(
         ctx: CliContext,
         expression: str,
+        token: Optional[str],
+        api_url: Optional[str],
         command: bool,
         output_format: Optional[str],
         timeout_sec: float,
@@ -56,7 +64,16 @@ def execute(
         except ValueError:
             pass
 
-    api = ctx.upsolver_api()
+    if token and api_url:
+        api_url = parse_url(api_url)
+        assert api_url.is_absolute()
+        api = ctx.upsolver_api(ProfileAuthSettings(token, api_url))
+    elif token or api_url:
+        raise ConfigErr("Please provide api-url with token or use the `configure` sub-command "
+                        "to create a proper profile and use it with -p flag.")
+    else:
+        api = ctx.upsolver_api()
+
     if dry_run:
         check_result = api.check_syntax(expression)
         if len(check_result) > 0:
