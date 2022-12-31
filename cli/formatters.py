d024dda3a686d063f073823a53b283aed5ff483a
Jason Fine <jasonf20@gmail.com>
2022-12-31 20:46:42 +0200

    Revert "[Refactor]: Changed output formatters to be classes"
    This reverts commit 83217923b3f9ce3949dfbceb5add8771a1295800.

--- cli/formatters.py
+++ cli/formatters.py
@@ -5,17 +5,14 @@ import json
 from enum import Enum
 from functools import partial
 from json import JSONEncoder
-from typing import Any, Callable, Optional, Protocol
+from typing import Any, Callable, Optional
 
 from tabulate import tabulate
 
 from cli import errors
 from cli.utils import flatten
 
-
-class Formatter(Protocol):
-    def do_fmt(self, value: Any) -> str:
-        raise NotImplementedError("Missing implemenation of fmt")
+Formatter = Callable[[Any], str]
 
 
 class OutputFmt(Enum):
@@ -26,13 +23,13 @@ class OutputFmt(Enum):
 
     def get_formatter(self) -> Formatter:
         if self == OutputFmt.JSON:
-            return JsonFormatter()
+            return fmt_json
         elif self == OutputFmt.CSV:
-            return CsvFormatter()
+            return fmt_csv()
         elif self == OutputFmt.TSV:
-            return CsvFormatter().withDelimiter('\t')
+            return fmt_csv(delimiter='\t')
         elif self == OutputFmt.PLAIN:
-            return PlainFormatter()
+            return fmt_plain
         else:
             raise errors.InternalErr(f'Unsupported output format: {self}')
 
@@ -48,23 +45,22 @@ def to_dict_maybe(o: Any) -> Optional[dict]:
         return None
 
 
-class JsonFormatter():
-    def do_fmt(self, value: Any) -> str:
-        class DataclassesJSONEncoder(JSONEncoder):
-            def default(self, o: Any) -> dict:
-                if dataclasses.is_dataclass(o):
-                    return dataclasses.asdict(o)
-                return super().default(o)
+def fmt_json(x: Any) -> str:
+    class DataclassesJSONEncoder(JSONEncoder):
+        def default(self, o: Any) -> dict:
+            if dataclasses.is_dataclass(o):
+                return dataclasses.asdict(o)
+            return super().default(o)
 
-        def dumps(y: Any) -> str:
-            return json.dumps(y, cls=DataclassesJSONEncoder, indent=2)
+    def dumps(y: Any) -> str:
+        return json.dumps(y, cls=DataclassesJSONEncoder, indent=2)
 
-        if type(value) is list:
-            return '\n'.join([dumps(xx) for xx in value])
-        elif type(value) is str:
-            return value
-        else:
-            return dumps(value)
+    if type(x) is list:
+        return '\n'.join([dumps(xx) for xx in x])
+    elif type(x) is str:
+        return x
+    else:
+        return dumps(x)
 
 
 def fmt_any(x: Any, fmt_list: Callable[[list], str]) -> str:
@@ -83,19 +79,12 @@ def to_dict_or_raise(x: Any, desired_fmt: OutputFmt) -> dict:
     return maybe_dict
 
 
-class CsvFormatter():
+def fmt_csv(delimiter: str = ',') -> Callable[[Any], str]:
+    to_dict: Callable[[Any], dict] = partial(to_dict_or_raise, desired_fmt=OutputFmt.CSV)
 
-    delimiter = ','
-
-    def withDelimiter(self, delimiter: chr):
-        self.delimiter = delimiter
-        return self
-
-    def fmt_list(self, xs: list) -> str:
+    def fmt_list(xs: list) -> str:
         if len(xs) == 0:
             return ''
-
-        to_dict: Callable[[Any], dict] = partial(to_dict_or_raise, desired_fmt=OutputFmt.CSV)
 
         dicts = [flatten(to_dict(x)) for x in xs]
         keys = sorted(set().union(*dicts))
@@ -103,7 +92,7 @@ class CsvFormatter():
         with io.StringIO() as o:
             w = csv.DictWriter(
                 o,
-                delimiter=self.delimiter,
+                delimiter=delimiter,
                 fieldnames=keys,
                 quoting=csv.QUOTE_NONNUMERIC,
                 restval="<null>"
@@ -114,15 +103,14 @@ class CsvFormatter():
                 w.writerow(x)
             return o.getvalue()
 
-    def do_fmt(self, value: Any) -> str:
-        return fmt_any(value, fmt_list=self.fmt_list)
+    return partial(fmt_any, fmt_list=fmt_list)
 
 
-class PlainFormatter():
+def fmt_plain(x: Any) -> str:
+    to_dict: Callable[[Any], dict] = \
+        partial(to_dict_or_raise, desired_fmt=OutputFmt.PLAIN)
 
-    def fmt_list(self, xs: list) -> str:
-        to_dict: Callable[[Any], dict] = \
-            partial(to_dict_or_raise, desired_fmt=OutputFmt.PLAIN)
+    def fmt_list(xs: list) -> str:
         if len(xs) == 0:
             return ''
 
@@ -131,8 +119,7 @@ class PlainFormatter():
             headers=list(to_dict(xs[0]))
         )
 
-    def do_fmt(self, value: Any) -> str:
-        return fmt_any(value, fmt_list=self.fmt_list)
+    return fmt_any(x, fmt_list)
 
 
 def get_output_format(output_format: Optional[str]) -> OutputFmt:
